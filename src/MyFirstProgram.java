89399640954e73ada3db61d26a7bde44e1f078a5
18391028 <68807608+18391028@users.noreply.github.com>
2022-02-25 18:47:19 +1100

    Initial commit

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,9 @@
+// This is the first program in the Gaddis textbook.
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
