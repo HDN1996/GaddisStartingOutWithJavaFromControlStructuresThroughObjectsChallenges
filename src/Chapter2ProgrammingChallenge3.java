313f004cad71336a59c9949b741a8f9f169cef81
HDN <workHDN2019@gmail.com>
2022-02-25 19:23:53 +1100

    Chapter 2 Programming Challenge 3

---
+++ src/Chapter2ProgrammingChallenge3.java
@@ -0,0 +1,20 @@
+
+public class Chapter2ProgrammingChallenge3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nameMember;
+		double membershipFee;
+		String registrationDate;
+		String expiryDate;
+		
+		nameMember = "John Smith";
+		membershipFee = 3.50;
+		registrationDate = "01/01/2001";
+		expiryDate = "31/12/2099";
+		
+		System.out.println("The name of the member is " + nameMember + ".\nTheir membership fee is $" + membershipFee + ".\nTheir"
+				+ " registration date is " + registrationDate + ".\nTheir expiry date is " + expiryDate + ".");
+	}
+
+}
