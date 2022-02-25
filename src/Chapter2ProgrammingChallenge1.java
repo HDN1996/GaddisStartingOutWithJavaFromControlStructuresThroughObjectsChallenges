6465e3608c8c293406e420bb185c04cf23361757
18391028 <68807608+18391028@users.noreply.github.com>
2022-02-25 19:04:30 +1100

    Chapter 2 Programming Challenge 1

---
+++ src/Chapter2ProgrammingChallenge1.java
@@ -0,0 +1,9 @@
+public class Chapter2ProgrammingChallenge1 {
+	public static void main(String[] args) {
+		// Declare the three given variables.
+		String name="John Smith";
+		int age=18;
+		double annualPay=86400;
+		System.out.println("My name is " + name + ", my age is " + age + " and I hope to earn $" + annualPay + " per year.");
+	}
+}
