3657c03c77e4c86d3dcbf040639100ae6fafeafd
18391028 <workHDN2019@gmail.com>
2022-02-25 19:14:46 +1100

    Chapter 2 Programming Challenge 2

---
+++ src/Chapter2ProgrammingChallenge2.java
@@ -0,0 +1,13 @@
+
+public class Chapter2ProgrammingChallenge2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String productName = "video-game";
+		String itemCode = "A101";
+		String CustomerId = "VD112";
+		System.out.println("The product name is " + productName + ", the item code is " + itemCode + " and the Customer ID is " + CustomerId +
+				".");
+	}
+
+}
