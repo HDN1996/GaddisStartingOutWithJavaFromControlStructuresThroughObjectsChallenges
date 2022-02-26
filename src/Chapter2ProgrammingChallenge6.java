8375691482fe6d2ddbc3752559fe963fe44d1796
HDN <workHDN2019@gmail.com>
2022-02-26 15:49:23 +1100

    Chapter 2 Programming Challenge 6

---
+++ src/Chapter2ProgrammingChallenge6.java
@@ -0,0 +1,12 @@
+
+public class Chapter2ProgrammingChallenge6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double height = 8;
+		double radius = 7;
+		double cylinderVolume= 3.14 * Math.pow(radius, 2) * height;
+		System.out.println("The volume of the cylinder is approximately " + cylinderVolume + "cm^3.");
+	}
+
+}
