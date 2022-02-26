dbd6eb7f198a856bab99714fc1683ea440d8bc22
HDN <workHDN2019@gmail.com>
2022-02-26 15:36:16 +1100

    Chapter 2 Programming Challenge 5

---
+++ src/Chapter2ProgrammingChallenge4.java
@@ -0,0 +1,9 @@
+
+public class Chapter2ProgrammingChallenge4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("A\nAE\nAEI\nAEIO\nAEIOU");
+	}
+
+}
---
+++ src/Chapter2ProgrammingChallenge5.java
@@ -0,0 +1,11 @@
+
+public class Chapter2ProgrammingChallenge5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PERCENT = 0.62;
+		double sales = 4600000;
+		System.out.println("The East coast division will make $" + PERCENT*sales + " in sales this year.");
+	}
+
+}
