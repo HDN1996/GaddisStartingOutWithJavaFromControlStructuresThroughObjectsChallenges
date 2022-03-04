3f40f97e53d37680e60a2501ce1f7d276d35efce
HDN <workHDN2019@gmail.com>
2022-03-04 17:36:19 +1100

    Chapter 2 Programming Challenge 17

--- src/Chapter2ProgrammingChallenge16.java
+++ src/Chapter2ProgrammingChallenge16.java
@@ -3,7 +3,13 @@ public class Chapter2ProgrammingChallenge16 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		final int CUSTOMERS = 12467;
+		final double CUSTOMER_PERCENTAGE=0.14;
+		final double CITRUS_PERCENTAGE=0.64;
+		System.out.printf("The approximate number of customers who purchase one or more energy drinks per "
+				+ "week is %.0f.\n", CUSTOMERS*CUSTOMER_PERCENTAGE);
+		System.out.printf("The approximate number of customers who prefer citrus-flavoured energy drinks is"
+				+ " %.0f.\n", CUSTOMERS*CUSTOMER_PERCENTAGE*CITRUS_PERCENTAGE);
 	}
 
 }
